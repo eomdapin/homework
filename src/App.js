4d499f65d5f8f8cb0df807585d1342cf79491615
Eomdapin <qlsekdja2@gmail.com>
2023-10-09 19:04:21 +0900

    last

--- src/App.js
+++ src/App.js
@@ -1,46 +1,90 @@
 import './App.css';
+import React from 'react';
 
 const Title = (props) => {
   return (<h1>{props.children}</h1>);
 };
 
-const AddProduct = () => {
+const AddProduct = ({ next_img, now_img, prev_img, add_product }) => {
   return (
     <div id="AddProduct">
       <form action="">
-        <img src="./img/1.jpg" alt="keyboard 1" />
-        <input className="price" type="text" placeholder="가격 입력"></input>
-        <input className="next_btn" type="button" value="<"></input>
-        <input className="add_btn" type="button" value="상품 추가"></input>
-        <input className="next_btn" type="button" value=">"></input><br />
+        <img src={now_img} alt="keyboard" />
+        <input className="next_btn" type="button" value="<" onClick={prev_img}></input>
+        <input className="add_btn" type="submit" value="상품 추가" onClick={add_product}></input>
+        <input className="next_btn" type="button" value=">" onClick={next_img}></input><br />
       </form>
     </div>
   );
 };
 
-const Product = () => {
+const Product = ({ src }) => {
   return (
     <div className="Product">
-      <img src="/img/1.jpg" alt="keyboard 1" />
-      <span> 가격 </span>
+      <img src={src} alt="keyboard"  />
     </div>
   );
 };
 
-const ProductList = () => {
+const ProductList = ({ product }) => {
+  let key = Date.now();
   return(
     <div id="ProductList">
-      <Product />
+      {product.map(item => {
+        return <Product src={item} key={key++}/>;
+      })}
     </div>
   );
+};
+
+const jsonLocal = {
+  setItem: (key, value) => {
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+  getItem: key => JSON.parse(localStorage.getItem(key)),
 };
 
 function App() {
+  const img_list = ['./img/1.jpg', './img/2.png', './img/3.jpg', './img/4.jpg', './img/5.png', './img/6.jpg', './img/7.jpg'];
+  const [now_img, setNow_img] = React.useState(img_list[0]);
+  const [product, setProduct] = React.useState(() => {
+    return jsonLocal.getItem('product') || [];
+  });
+
+  function add_product(event) {
+    event.preventDefault();
+    const next_Product = [...product, now_img];
+    jsonLocal.setItem('product', next_Product);
+    return setProduct(next_Product);
+  }
+
+  function next_img() {
+    setNow_img((pre) => {
+      let img_num = img_list.indexOf(pre);
+      if(img_num === 6) {
+        img_num = 5;
+      }
+      img_num++;
+      return img_list[img_num];
+    });
+  }
+  
+  function prev_img() {
+    setNow_img((pre) => {
+      let img_num = img_list.indexOf(pre);
+      if(img_num === 0) {
+        img_num = 1;
+      }
+      img_num--;
+      return img_list[img_num];
+    });
+  }
+
   return (
     <div className="App">
-      <Title>상품페이지</Title>
-      <AddProduct />
-      <ProductList />
+      <Title>상품을 추가하세요</Title>
+      <AddProduct now_img={now_img} next_img={next_img} prev_img={prev_img} add_product={add_product} />
+      <ProductList product={product} />
     </div>
   );
 }
